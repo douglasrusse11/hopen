74c4662dc08f40a974555ac1966d12372524e9b8
Douglas Russell <douglas.russell7@btinternet.com>
2022-03-11 13:00:34 +0000

    adds more tests for Header

--- src/components/Header.spec.js
+++ src/components/Header.spec.js
@@ -2,20 +2,31 @@ import { render, fireEvent } from '@testing-library/react';
 import Header from './Header';
 import { MemoryRouter } from 'react-router-dom';
 import {useHref} from 'react-router';
+import { useTranslation } from 'react-i18next';
+import { act } from 'react-dom/test-utils';
 
 
 const mockSetDisplayMenu = jest.fn();
 const mockSetDisplayLogin = jest.fn();
+const mockUseHref = jest.fn();
 
 jest.mock('react-router', () => {
-    const originalModule = jest.requireActual('react-router');
-
     return {
-        __esModule: true,
-        ...originalModule,
-        useHref: jest.fn()
+        ...jest.requireActual('react-router'),
+        useHref: (to) => {mockUseHref(to)}
         }
     })
+
+const mockChangeLanguage = jest.fn();
+
+jest.mock('react-i18next', () => {
+    return {
+        ...jest.requireActual('react-i18next'),
+        useTranslation: () => ({
+            t: (text) => text === 'home.signin' ? 'home.signin' : 'home.signout', 
+            i18n: {changeLanguage: mockChangeLanguage}})
+    }
+})
 
 const props = {
     user: null,
@@ -45,7 +56,7 @@ describe('Header', () => {
     it('should link to home when home button is clicked', () => {
         const { getByText } = render(<Header {...props} />, {wrapper: MemoryRouter}) 
          fireEvent.click(getByText(/hopen/i));
-         expect(useHref).toHaveBeenCalledWith('/');
+         expect(mockUseHref).toHaveBeenCalledWith('/');
     })
 
     it('should have a language selector', () => {
@@ -53,16 +64,34 @@ describe('Header', () => {
         expect(getByTestId('rfs')).toBeInTheDocument()
     })
 
+    it('should be able to select a language', () => {
+        const { getByTestId, getAllByRole, getByText } = render(<Header {...props} />, {wrapper: MemoryRouter}) 
+        fireEvent.click(getByTestId('rfs-btn'))
+        fireEvent.click(getByText(/english/ig))
+        expect(mockChangeLanguage).toHaveBeenCalledWith('GB')
+    })
+
     it('should have a Sign In button', () => {
         const { getByText } = render(<Header {...props} />, {wrapper: MemoryRouter}) 
         expect(getByText('home.signin')).toBeInTheDocument()
     })
 
-    it('should have a Sign Out button if use is signed in', () => {
-        const newProps = {...props, user: {}}
-        const { getByText } = render(<Header {...newProps} />, {wrapper: MemoryRouter}) 
-        expect(getByText('home.signout')).toBeInTheDocument()
+    it('should call setDisplayLogin, when Sign In button is clicked', () => {
+        const { getByText } = render(<Header {...props} />, {wrapper: MemoryRouter}) 
+        fireEvent.click(getByText('home.signin'))
+        expect(mockSetDisplayLogin).toHaveBeenCalled();
     })
 
+    it('should display login when displayLogin is true', () => {
+        const loginProps = {...props, displayLogin: true};
+        const { getByRole } = render(<Header {...loginProps} />, {wrapper: MemoryRouter}) 
+        expect(getByRole('dialog')).toBeInTheDocument()
+    })
+
+    it('should have a Sign Out button if user is signed in', () => {
+        const userProps = {...props, user: {}}
+        const { getByText } = render(<Header {...userProps} />, {wrapper: MemoryRouter}) 
+        expect(getByText('home.signout')).toBeInTheDocument()
+    })
 
 });
